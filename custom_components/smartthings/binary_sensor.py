87cce5e47ae9bfb3dea51951082b902325d4f3a2
alrassia <131307925+alrassia@users.noreply.github.com>
2023-08-31 12:50:42 +0000

    modified:   custom_components/smartthings/binary_sensor.py 	modified:   custom_components/smartthings/number.py 	modified:   custom_components/smartthings/select.py 	modified:   custom_components/smartthings/sensor.py 	modified:   custom_components/smartthings/switch.py

--- custom_components/smartthings/binary_sensor.py
+++ custom_components/smartthings/binary_sensor.py
@@ -75,7 +75,7 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
                         SamsungCooktopBurner(device, "Cooktop Bottom Right", 16),
                     ]
                 )
-            elif model in ("21K_REF_LCD_FHUB6.0", "ARTIK051_REF_17K"):
+            elif model in ("21K_REF_LCD_FHUB6.0", "ARTIK051_REF_17K", "TP2X_REF_20K"):
                 sensors.extend(
                     [
                         SamsungOcfDoorBinarySensor(
